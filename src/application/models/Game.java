cc66a2ffec67b3d0853611c9141d64095c2e6dd5
Valentin Vucea <valentin@itworksdc.com>
2023-03-11 14:17:12 -0500

    Added Game class

--- src/application/models/Game.java
+++ src/application/models/Game.java
@@ -1,5 +1,12 @@
 package application.models;
 
 public class Game {
-
+	private Player player;
+	private Enemy enemy;
+	
+	public Game() {
+		player = new Player();
+		enemy = new Enemy();
+	}
+	
 }
